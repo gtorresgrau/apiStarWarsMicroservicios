639986f7ce39e5a448f3e42928d4111799c5efb2
Gonzalo Torres Grau <gonzalotorresgrau@gmail.com>
2023-09-18 01:44:55 -0300

    docker

--- gateway/index.js
+++ gateway/index.js
@@ -10,21 +10,22 @@ app.use(morgan('dev')),
 app.use(
     '/characters', 
     createProxyMiddleware({
-        target:'http://localhost:8001',
+        target:'http://characters:8001',
         changeOrigin:true,
     })   
 ); 
 app.use(
     '/films', 
     createProxyMiddleware({
-        target:'http://localhost:8002',
+        target:'http://films:8002',
         changeOrigin:true,
     })   
 ); 
+
 app.use(
     '/planets', 
     createProxyMiddleware({
-        target:'http://localhost:8003',
+        target:'http://planets:8003',
         changeOrigin:true,
     })   
 ); 
