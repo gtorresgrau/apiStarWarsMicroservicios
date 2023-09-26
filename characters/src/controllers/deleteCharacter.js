81d5b6a1f5c380ead1568017afebe506a1f56d93
Gonzalo Torres Grau <gonzalotorresgrau@gmail.com>
2023-09-26 00:37:00 -0300

    delete

--- characters/src/controllers/deleteCharacter.js
+++ characters/src/controllers/deleteCharacter.js
@@ -6,11 +6,9 @@ module.exports = async (req,res) => {
     const { id } = req.params;
     if(id) {
       const character = await Character.delete(id);
-      if(!character) {
-        response(res,200,'character not found');
-      }
-      response(res,200,'The Character was removed');
+      if(!character) response(res,404,character,'character not found');
+      response(res,200,character,'The Character was removed');
     }else{
-        response(res,400,'You cant delete wihtout an ID')
+        response(res,400,id,'You cant delete wihtout an ID')
     }
   };
