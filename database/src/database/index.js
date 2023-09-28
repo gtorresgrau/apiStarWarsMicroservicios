3d8094e11c1768e0e0e696f0320bf23fd7f879cd
Gonzalo Torres Grau <gonzalotorresgrau@gmail.com>
2023-09-28 01:47:45 -0300

    envs

--- database/src/database/index.js
+++ database/src/database/index.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb+srv://admin:admin@cluster0.pfpdqer.mongodb.net/star_wars';
 const character = require("./schemas/characterSchema");
 const film = require('./schemas/filmSchema');
 const planet = require('./schemas/planetSchema');
-//const {MONGO_URI} = require('../config/index')
+const {MONGO_URI} = require('../config/envs');
 
 const conn = mongoose.createConnection(MONGO_URI)
+console.log('MongoUri:',MONGO_URI);
+
 
 module.exports = {
     Character: conn.model("Character", character ),
