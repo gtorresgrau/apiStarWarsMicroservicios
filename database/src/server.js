b94feed36a5afc86a582f1cea0c30d09433bb72f
Gonzalo Torres Grau <gonzalotorresgrau@gmail.com>
2023-10-01 01:54:22 -0300

    cors

--- database/src/server.js
+++ database/src/server.js
@@ -8,6 +8,13 @@ server.use(morgan('dev'));
 
 server.use(require('./routes'));
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 server.use("*", (req, res) => {
     res.status(404).json({
       error: true,
