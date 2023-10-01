1ecfef2d618cdd67ecebf05b153fc0994981bff6
Gonzalo Torres Grau <gonzalotorresgrau@gmail.com>
2023-10-01 02:13:33 -0300

    mongo

--- database/src/database/index.js
+++ database/src/database/index.js
@@ -6,7 +6,7 @@ const planet = require('./schemas/planetSchema');
 const {MONGO_URI} = require('../config/envs');
 
 const conn = mongoose.createConnection(MONGO_URI)
-console.log('MongoUri:',MONGO_URI);
+//console.log('MongoUri:',MONGO_URI);
 
 
 module.exports = {
